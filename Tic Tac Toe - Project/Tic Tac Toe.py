d280533b305ab286a7118e04b6d2a25c9ebd0b31
Jonathan <julli626@gmail.com>
2025-01-16 23:09:20 +0200

    Project finished?

--- Tic Tac Toe - Project/Tic Tac Toe.py
+++ Tic Tac Toe - Project/Tic Tac Toe.py
@@ -7,12 +7,66 @@ def switch_players(turn, is_win_or_tie):
         turn[0], turn[1] = turn[1], turn[0]
         return turn
 
+def check_win_or_tie(is_win_or_tie, player_turn, board):
+    if is_win_or_tie == "W":
+        global player_won
+        player_won = player_turn
+        if player_won == p1_name:
+            game_score[0] += 1
+        elif player_won == p2_name:
+            game_score[1] += 1
+        print_board(board)
+        return "Win"
+    elif is_win_or_tie == "T":
+        print_board(board)
+        return "Tie"
+    elif is_win_or_tie == 0:
+        return "NoWin"
+
+def condition_win_or_tie(board):
+    win_conditions = [
+        (0, 1, 2),
+        (3, 4, 5),
+        (6, 7, 8),
+        (0, 3, 6),
+        (1, 4, 7),
+        (2, 5, 8),
+        (0, 4, 8),
+        (2, 4, 6)
+    ]
+
+    for index1, index2, index3 in win_conditions:#check win
+        if board[index1] == board[index2] == board[index3] and board[index1] != ' ':
+            # if index1
+            if board[index1] == 'X':
+                mark = 'X̶'
+                board[index1] = board[index2] = board[index3] = mark
+            else:
+                mark = 'O̶'
+                board[index1] = board[index2] = board[index3] = mark
+            return "W"
+
+    counter = 0 #check tie
+    for i in board:
+        if i != ' ':
+            counter += 1
+    if counter == 9:
+        return "T"
+    return 0
+
 def cpu_choice(board):
     while True:
         index_random = random.randint(1,9)
         for index, value in enumerate(board):
             if (index + 1) == index_random and value == ' ':
                 return index_random - 1
+
+def print_board(board):
+    print("\nBoard:")
+    for index, value in enumerate(board):
+            print(f"[{value}]", end=" ")
+            if (index + 1) % 3 == 0:
+                print()
 
 def pick_place(board, turn, P1, P2):
     game_status = "NoWin"
@@ -72,59 +126,17 @@ def pick_place(board, turn, P1, P2):
         if game_status != "NoWin":
             return game_status
 
-def check_win_or_tie(is_win_or_tie, player_turn, board):
-    if is_win_or_tie == "W":
-        global player_won
-        player_won = player_turn
-        if player_won == p1_name:
-            game_score[0] += 1
-        elif player_won == p2_name:
-            game_score[1] += 1
-        print_board(board)
-        return "Win"
-    elif is_win_or_tie == "T":
-        print_board(board)
-        return "Tie"
-    elif is_win_or_tie == 0:
-        return "NoWin"
-
-def print_board(board):
-    print("\nBoard:")
-    for index, value in enumerate(board):
-            print(f"[{value}]", end=" ")
-            if (index + 1) % 3 == 0:
-                print()
-
-def condition_win_or_tie(board):
-    win_conditions = [
-        (0, 1, 2),
-        (3, 4, 5),
-        (6, 7, 8),
-        (0, 3, 6),
-        (1, 4, 7),
-        (2, 5, 8),
-        (0, 4, 8),
-        (2, 4, 6)
-    ]
-
-    for index1, index2, index3 in win_conditions:#check win
-        if board[index1] == board[index2] == board[index3] and board[index1] != ' ':
-            # if index1
-            if board[index1] == 'X':
-                mark = 'X̶'
-                board[index1] = board[index2] = board[index3] = mark
-            else:
-                mark = 'O̶'
-                board[index1] = board[index2] = board[index3] = mark
-            return "W"
-
-    counter = 0 #check tie
-    for i in board:
-        if i != ' ':
-            counter += 1
-    if counter == 9:
-        return "T"
-    return 0
+def choose_symbol():
+    player1_symbol = input(f"\n{p1_name}, Choose Symbol. X or O \nAny other input will choose your symbol at random: ").upper()
+    if (player1_symbol == 'X') or (player1_symbol == 'O'):
+        return player1_symbol
+    else:
+        if random.randint(1,2) == 1:
+            player1_symbol = 'X'
+        else:
+            player1_symbol = 'O'
+        print("\nInvalid input. A symbol has been chosen for you.")
+        return player1_symbol
 
 def play_game():
     board_list = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '] #the board
@@ -147,38 +159,6 @@ def play_game():
             return True
         elif user_choise == "Q":
             return True
-
-def start_game():
-    print("Welcome to Tic Tac Toe!")
-    choose_name() #name choice
-    global game_score
-    game_score = [0, 0]
-    while True:
-        finished_game = play_game()
-        if finished_game:#checks if game is finished or not
-            play_again = input("\nDo you want to play again? Y for yes, everything else for no: ").lower()
-            if play_again != "y":
-                break
-    print(f"\nGame ended. Final score:")
-    print(f"{p1_name}: {game_score[0]} Points\n{p2_name}: {game_score[1]} Points")
-    if game_score[0] > game_score[1]:
-        print(f"\n{p1_name} is the winner!")
-    elif game_score[1] > game_score[0]:
-        print(f"\n{p2_name} is the winner!")
-    else:
-        print("\nTie! No one won!")
-
-def choose_symbol():
-    player1_symbol = input(f"\n{p1_name}, Choose Symbol. X or O \nAny other input will choose your symbol at random: ").upper()
-    if (player1_symbol == 'X') or (player1_symbol == 'O'):
-        return player1_symbol
-    else:
-        if random.randint(1,2) == 1:
-            player1_symbol = 'X'
-        else:
-            player1_symbol = 'O'
-        print("\nInvalid input. A symbol has been chosen for you.")
-        return player1_symbol
 
 def choose_name():
     global p1_name
@@ -204,14 +184,13 @@ def choose_name():
                     p2_name = input("\nPlayer 2 what's your Name? ")
                     try:
                         valid_p2_name = False
-                        if p2_name.isalpha():
+                        if p2_name.isalpha() and p2_name != p1_name:
                             valid_p2_name = True
                             break
                         else:
                             raise
                     except:
-                        print("Make sure the name consists of letters only!")
-
+                        print("Make sure the name is different from Player 1's name, and consists of letters only!")
             elif man_v_machine == 'PC':#sets player 1 to play against the PC
                 p2_name = "CPU"
                 break
@@ -222,5 +201,28 @@ def choose_name():
             print(f"{e}\n")
         if valid_p2_name:
             break
+
+def start_game():
+    print("Welcome to Tic Tac Toe!")
+    choose_name() #name choice
+    global game_score
+    game_score = [0, 0]
+    while True:
+        finished_game = play_game()
+        if finished_game:#checks if game is finished or not
+            play_again = input("\nDo you want to play again? Y for yes, everything else for no: ").lower()
+            if play_again != "y":
+                break
+    print(f"\nGame ended. Final score:")
+    print(f"{p1_name}: {game_score[0]} Points\n{p2_name}: {game_score[1]} Points")
+    if game_score[0] > game_score[1]:
+        print(f"\n{p1_name} is the winner!")
+    elif game_score[1] > game_score[0]:
+        print(f"\n{p2_name} is the winner!")
+    else:
+        print("\nTie! No one won!")
+
+
+
 if __name__ == '__main__':
     start_game()
